ebab1774c47bbde8b8a64c34741342ba4e3a3e3a
Louie Moretti <morettilouie@gmail.com>
2022-10-08 02:39:53 -0500

    form validation and screen handling based on whats clicked

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,17 +6,15 @@ function Nav(props) {
     const {
         categories = [],
         setCurrentCategory,
-        currentCategory
+        currentCategory,
+        contactSelected,
+        setContactSelected
     } = props
 
     useEffect(() => {
         document.title = capitalizeFirstLetter(currentCategory.name);
     }, [currentCategory]);
 
-    const handleClick = (item) => {
-        console.log(item);
-        return item;
-    };
 
     return (
         <header className="flex-row px-1">
@@ -32,23 +30,21 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a
-                            href="#about"
-                        >
-                            About me
-                        </a>
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>About me</a>
                     </li>
-                    <li className={"mx-2"}>
-                        <span onClick={() => handleClick('Contact')}>
+                    <li className={`mx-2 ${contactSelected && 'naveActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>
                             Contact
                         </span>
                     </li>
                     {categories.map((category) => (
-                        <li className={`mx-1 ${currentCategory.name === category.name
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && !contactSelected && `navActive`
                             }`} key={category.name}>
                             <span
                                 onClick={() => {
-                                    setCurrentCategory(category)
+                                    setCurrentCategory(category);
+                                    setContactSelected(false);
                                 }}
                             >
                                 {capitalizeFirstLetter(category.name)}
